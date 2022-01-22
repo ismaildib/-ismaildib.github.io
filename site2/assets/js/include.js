376efce051ff716eaaba882ffc150860b7e7b9c8
ismaildib <36337329+ismaildib@users.noreply.github.com>
2022-01-22 23:22:14 +0100

    test

---
+++ site2/assets/js/include.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",function(){
+	
+	let e=document.getElementsByTagName("include");
+	while(e.length){
+		fetch(e[0].attributes.src.value)
+		.then((response)=>{return response.text();})
+		.then((html)=>{
+			e[0].insertAdjacentHTML("afterend",html);
+			e[0].remove();
+		});
+	}
+	
+	});
+	
+	
+	
