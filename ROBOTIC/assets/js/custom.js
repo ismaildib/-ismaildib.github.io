3e4192ea82cab5c531041aeca6d52e12715d0c92
ismaildib <36337329+ismaildib@users.noreply.github.com>
2022-01-25 17:39:32 +0100

    Update custom.js

--- ROBOTIC/assets/js/custom.js
+++ ROBOTIC/assets/js/custom.js
@@ -2,7 +2,7 @@ var url1 = "https://script.google.com/macros/s/AKfycbyO2P-2hDC4DDCcOBSa7GecXiu68
 function Subscribe() {
   var x = document.getElementById("subscribeEmail").value;
   document.getElementById("subscribeEmail").value="";
-  var url2 = url1 + "add_email=" + x + "&talk=ismail";
+  var url2 = url1 + "add_email=" + x ;//+ "&talk=ismail";
 	fetch (url2)
 	.then(y => y.text())
 	.then(z => alert(z));
