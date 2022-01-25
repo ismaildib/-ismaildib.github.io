44901763446c38bb71b1b205722ed78cfdbebf7a
ismaildib <36337329+ismaildib@users.noreply.github.com>
2022-01-25 17:04:08 +0100

    Update custom.js

--- ROBOTIC/assets/js/custom.js
+++ ROBOTIC/assets/js/custom.js
@@ -1,5 +1,9 @@
+url1 = "https://script.google.com/macros/s/AKfycbyO2P-2hDC4DDCcOBSa7GecXiu68dEGqJbnuVJRalgt2fTt0ygLzqzutz_qKznrIycp/exec?";
 function Subscribe() {
   var x = document.getElementById("subscribeEmail").value;
-  alert(x);
   document.getElementById("subscribeEmail").value="";
+  let url2 = url1 + "add_email=" + x + "&talk=ismail";
+	fetch (url1)
+	.then(x => x.text())
+	.then(y => alert(x););
 }
